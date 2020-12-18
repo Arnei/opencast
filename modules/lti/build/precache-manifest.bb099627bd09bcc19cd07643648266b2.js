1639bcb5e33ac8f00e2e57a37beffedf3cd428aa
lwillmann <willmann@elan-ev.de>
2020-12-18 15:39:58 +0100

    Editor rebase from 9 to 8

---
+++ modules/lti/build/precache-manifest.bb099627bd09bcc19cd07643648266b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8cacc485fc689739d30b9beaab975ed",
+    "url": "/ltitools/index.html"
+  },
+  {
+    "revision": "14375987536db927d683",
+    "url": "/ltitools/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "74f9fce410b36370d8bc",
+    "url": "/ltitools/static/css/main.7e1579d1.chunk.css"
+  },
+  {
+    "revision": "14375987536db927d683",
+    "url": "/ltitools/static/js/2.2ff7ccf3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ltitools/static/js/2.2ff7ccf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f9fce410b36370d8bc",
+    "url": "/ltitools/static/js/main.ea51578b.chunk.js"
+  },
+  {
+    "revision": "efcf3580c4babd90b821",
+    "url": "/ltitools/static/js/runtime-main.fe9effc2.js"
+  }
+]);

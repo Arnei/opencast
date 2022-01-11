9c919905f97754bff64df63021080850cea3ea04
Arnei <arnewilken@yahoo.de>
2022-01-11 11:02:25 +0100

    Remove workflow statistics from workflow rest and remote endpoints

--- modules/workflow-service-remote/src/main/java/org/opencastproject/workflow/remote/WorkflowServiceRemoteImpl.java
+++ modules/workflow-service-remote/src/main/java/org/opencastproject/workflow/remote/WorkflowServiceRemoteImpl.java
@@ -46,7 +46,6 @@ import org.opencastproject.workflow.api.WorkflowQuery;
 import org.opencastproject.workflow.api.WorkflowQuery.QueryTerm;
 import org.opencastproject.workflow.api.WorkflowService;
 import org.opencastproject.workflow.api.WorkflowSet;
-import org.opencastproject.workflow.api.WorkflowStatistics;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
@@ -314,26 +313,6 @@ public class WorkflowServiceRemoteImpl extends RemoteBase implements WorkflowSer
       closeConnection(response);
     }
     throw new WorkflowDatabaseException("Workflow instances can not be loaded from a remote workflow service");
-  }
-
-  /**
-   * {@inheritDoc}
-   *
-   * @see org.opencastproject.workflow.api.WorkflowService#getStatistics()
-   */
-  @Override
-  public WorkflowStatistics getStatistics() throws WorkflowDatabaseException {
-    HttpGet get = new HttpGet("/statistics.xml");
-    HttpResponse response = getResponse(get);
-    try {
-      if (response != null)
-        return WorkflowParser.parseWorkflowStatistics(response.getEntity().getContent());
-    } catch (Exception e) {
-      throw new WorkflowDatabaseException("Unable to load workflow statistics", e);
-    } finally {
-      closeConnection(response);
-    }
-    throw new WorkflowDatabaseException("Unable to connect to a remote workflow service");
   }
 
   /**
